9f53d56ec167dcee65f072507e6a3202d8981818
Logan <meltonlogan@me.com>
2022-09-26 09:15:24 -0500

    added EvenOddSort

---
+++ InterviewQuestions.playground/Pages/EvenOddSort.xcplaygroundpage/Contents.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+/*
+ Write a function that takes an array of Random Integers as the input and returns an array where all the Even numbers are at the beginning and all the Odd numbers are at the end. Solve with O(n) efficiency and O(1) memory.
+ Solve using an in-place algorithm, without adding an auxiliar array. More info at: https://en.wikipedia.org/wiki/In-place_algorithm
+ */
